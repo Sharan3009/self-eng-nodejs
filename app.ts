01e02b4c4af286bc46b261a419c68210236844d9
shasingh <sharandeep.singh@niceactimize.com>
2021-01-15 00:23:31 +0530

    morgan logging

--- app.ts
+++ app.ts
@@ -1,15 +1,34 @@
 import express from 'express';
 import config from './config/app-config';
 import morgan from 'morgan';
-import fs from 'fs';
+import rfs = require('rotating-file-stream');
 import path from 'path';
 
 const app:express.Application = express();
 
-const port = config.port;
+const port:number = config.port;
+
+const pad = (num:number):string => (num > 9 ? "" : "0") + num;
+
+var generator = (time:Date,index:number):string => {
+    const fileName:string = "debug.log";
+    if (!time) return fileName;
+    var year = time.getFullYear();
+    var month = pad(time.getMonth() + 1);
+    var date = pad(time.getDate());
+    var folder = `${year}-${month}-${date}`;
+
+    return `${folder}/${folder}-${index}-${fileName}`;
+};
+
+let accessLogStream:rfs.RotatingFileStream = rfs.createStream(<rfs.Generator>generator, {
+    interval: '1d', // rotate daily
+    size: "10M",
+    path: path.join(__dirname, 'logs'),
+})
 
 app.get("/",(req:express.Request,res:express.Response)=>{
-    res.send("Hello world")
+    res.send("Hello world");
 })
 
 if(config.env==="dev"){
@@ -18,7 +37,7 @@ if(config.env==="dev"){
 } else {
     // log all requests to debug.log
     app.use(morgan('common', {
-        stream: fs.createWriteStream(path.join(__dirname, 'debug.log'), { flags: 'a' })
+        stream: accessLogStream
     }))
 }
 
