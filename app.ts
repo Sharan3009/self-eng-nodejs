0f5c5b3e4c8e8b23bf6338106b7dd905427c4900
shasingh <sharandeep.singh@niceactimize.com>
2021-01-14 22:58:54 +0530

    added morgan

--- app.ts
+++ app.ts
@@ -1,11 +1,26 @@
 import express from 'express';
 import config from './config/app-config';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 const app:express.Application = express();
+
 const port = config.port;
+
 app.get("/",(req:express.Request,res:express.Response)=>{
     res.send("Hello world")
 })
+
+if(config.env==="dev"){
+    // log all respnses to console
+    app.use(morgan("dev"));
+} else {
+    // log all requests to debug.log
+    app.use(morgan('common', {
+        stream: fs.createWriteStream(path.join(__dirname, 'debug.log'), { flags: 'a' })
+    }))
+}
 
 app.listen(port,()=>{
     console.log(`Server is started at port:${port}`);
