2510e30f7669f4e6d5aee40e199d2f29fbcbd8e8
shasingh <sharandeep.singh@niceactimize.com>
2021-01-15 12:57:39 +0530

    added helmet, cookie parser and bod parser

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import config from './config/app-config';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import HttpRequestLogger from './utils/HttpRequestLogger';
 
 const app:express.Application = express();
@@ -9,6 +12,14 @@ const port:number = config.port;
 // start logging the requests
 const httpLogger = new HttpRequestLogger(app);
 httpLogger.start();
+
+// without bodyParser posting method cannot happen and had to be anywhere before routes function
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(helmet());
 
 app.get("/",(req:express.Request,res:express.Response)=>{
     res.send("Hello world");
