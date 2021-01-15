0cdefb043d5ab0f12c7b60a49c27c2b3b83d682a
shasingh <sharandeep.singh@niceactimize.com>
2021-01-15 13:28:26 +0530

    created startup util

--- app.ts
+++ app.ts
@@ -1,47 +1,20 @@
 import express from 'express';
 import config from './config/app-config';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import fs from 'fs';
-import HttpRequestLogger from './app/utils/httpRequestLogger';
+import AppStartUtil from './app/utils/appStartUtil';
 
 const app:express.Application = express();
 
 const port:number = config.port;
 
-// start logging the requests
-const httpLogger = new HttpRequestLogger(app);
-httpLogger.start();
+const startup = new AppStartUtil(app);
 
 // without bodyParser posting method cannot happen and had to be anywhere before routes function
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(cookieParser());
-
-app.use(helmet());
-
-// getting all models present in models folder
-let modelsPath = './app/models';
-if(fs.existsSync(modelsPath)){
-    fs.readdirSync(modelsPath).forEach(function(file){
-        if(~file.indexOf('js')){
-            require(`${modelsPath}/${file}`)
-        }
-    })
-}
-
-// getting all routes present in routes folder
-let routesPath = './app/routes';
-if(fs.existsSync(routesPath)){
-    fs.readdirSync(routesPath).forEach(function(file){
-        if(~file.indexOf('.js')){
-            let route = require(`${routesPath}/${file}`)
-            route.setRouter(app)
-        }
-    })
-}
+startup.setHttpLogger()
+.useBodyParser()
+.useCookieParser()
+.useHelmet()
+.includeModels()
+.includeRoutes();
 
 app.get("/",(req:express.Request,res:express.Response)=>{
     res.send("Hello world");
---
+++ app/utils/appStartUtil.ts
@@ -0,0 +1,65 @@
+import {Application} from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import fs from 'fs';
+import HttpRequestLogger from './httpRequestLogger';
+
+export default class AppStartUtil{
+
+    private app:Application;
+    private modelsPath:string = "../models";
+    private routesPath:string = "../routes";
+
+    constructor(app: Application){
+        this.app = app;
+    }
+
+    public useBodyParser = ():AppStartUtil => {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        return this;
+    }
+
+    public useCookieParser = ():AppStartUtil => {
+        this.app.use(cookieParser());
+        return this;
+    }
+
+    public useHelmet = ():AppStartUtil => {
+        this.app.use(helmet())
+        return this;
+    }
+
+    public includeModels = ():AppStartUtil => {
+        // getting all models present in models folder
+        if(fs.existsSync(this.modelsPath)){
+            fs.readdirSync(this.modelsPath).forEach((file)=>{
+                if(~file.indexOf('js')){
+                    require(`${this.modelsPath}/${file}`)
+                }
+            })
+        }
+        return this;
+    }
+
+    public includeRoutes = ():AppStartUtil => {
+        // getting all routes present in routes folder
+        if(fs.existsSync(this.routesPath)){
+            fs.readdirSync(this.routesPath).forEach((file)=>{
+                if(~file.indexOf('.js')){
+                    let route = require(`${this.routesPath}/${file}`)
+                    route.setRouter(this.app)
+                }
+            })
+        }
+        return this;
+    }
+
+    public setHttpLogger = ():AppStartUtil => {
+        // start logging the requests
+        const httpLogger:HttpRequestLogger = new HttpRequestLogger(this.app);
+        httpLogger.start();
+        return this;
+    }
+}
