48a2ed09031264879fbe10b273ede7293ee22b65
shasingh <sharandeep.singh@niceactimize.com>
2021-01-15 13:06:50 +0530

    added models and routes commit

--- app.ts
+++ app.ts
@@ -3,7 +3,8 @@ import config from './config/app-config';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
-import HttpRequestLogger from './app/utils/HttpRequestLogger';
+import fs from 'fs';
+import HttpRequestLogger from './app/utils/httpsRequestLogger';
 
 const app:express.Application = express();
 
@@ -20,6 +21,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(helmet());
+
+// getting all models present in models folder
+let modelsPath = './app/models';
+if(fs.existsSync(modelsPath)){
+    fs.readdirSync(modelsPath).forEach(function(file){
+        if(~file.indexOf('js')){
+            require(`${modelsPath}/${file}`)
+        }
+    })
+}
+
+// getting all routes present in routes folder
+let routesPath = './app/routes';
+if(fs.existsSync(routesPath)){
+    fs.readdirSync(routesPath).forEach(function(file){
+        if(~file.indexOf('.js')){
+            let route = require(`${routesPath}/${file}`)
+            route.setRouter(app)
+        }
+    })
+}
 
 app.get("/",(req:express.Request,res:express.Response)=>{
     res.send("Hello world");
