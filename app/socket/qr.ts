cfbe0c5b31b777c0dba6695e1a1101d33689d649
Sharan3009 <sharansingh3009@gmail.com>
2021-02-13 21:10:53 +0530

    qr

--- app/socket/qr.ts
+++ app/socket/qr.ts
@@ -2,22 +2,21 @@ import { Socket } from "socket.io";
 import qr from "qrcode";
 import response from "../utils/response";
 import { Response } from "../../Interface/Response";
+import { v5, v4 } from "uuid";
 
 export const generateQR = (socket:Socket) => {
     const channel:string = "GENERATE_QR";
-    socket.on(channel,(text:string)=>{
-        let resp:Response<any> = null;
-        qr.toDataURL(text,
-            {
-                margin:0
-            },
-            (err:Error,url:string)=>{
-            if(err){
-                resp = response.error(err.message);
-            } else {
-                resp = response.success(url);
-            }
-            socket.emit(channel,resp);
-        })
+    let resp:Response<any> = null;
+    qr.toDataURL(v5(socket.id,v4()),
+        {
+            margin:0
+        },
+        (err:Error,url:string)=>{
+        if(err){
+            resp = response.error(err.message);
+        } else {
+            resp = response.success(url);
+        }
+        socket.emit(channel,resp);
     })
 }
